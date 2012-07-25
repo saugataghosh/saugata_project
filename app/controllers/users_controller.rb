fd24d5b7546a9e173882e40048bbf76195617b0e
saugataghosh <saugataghosh90@gmail.com>
2012-07-26 02:56:15 +0530

    rails application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,20 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def create
-    User.create(params[:user])
-    redirect_to users_path
+    @user = User.new(params[:user])
+ 
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome Email after save
+        UserMailer.welcome_email(@user).deliver
+ 
+        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
-  def destroy
+    def destroy
     User.find(params[:id]).destroy
     redirect_to users_path
   end
